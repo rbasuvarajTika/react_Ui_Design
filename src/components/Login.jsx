252658821ee93821e1430c4c3f9fd001effd95fb
rbasuvaraj <rbasuvaraj@tikamobile.com>
2023-10-25 09:55:57 +0530

    Axios Chnages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import logo from "../assets/images/logo.jpeg"
 import { useState } from "react";
 import axios from "axios"
 import { ToastContainer, toast } from 'react-toast'
+import axiosBaseURL from './axios.js';
 
 function Login() {
     const navigate = useNavigate();
@@ -21,7 +22,7 @@ function Login() {
             console.log("kjbhbkhbhvbj");
             try {
                 setLoading(true)
-                await axios.post('https://dev.tika.mobi:8443/next-service/api/v1/auth/signin',
+                await axiosBaseURL.post('/api/v1/auth/signin',
                     JSON.stringify({ userName, password }),
                     {
                         headers: {
