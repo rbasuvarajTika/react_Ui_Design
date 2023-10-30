f90ffe6ad802d4d223814500ffe95d669f950f2c
rbasuvaraj <rbasuvaraj@tikamobile.com>
2023-10-30 11:17:45 +0530

    Chnages

--- src/components/case_details/CaseDetailsNew.jsx
+++ src/components/case_details/CaseDetailsNew.jsx
@@ -835,6 +835,9 @@ const zoomInSecond = () => {
   const handle_netSuitSubmission = () => {
     setNetSuit(true)
   }
+  const handle_netDeSubmission = () => {
+    setNetSuit(false)
+  }
 
   return (
       <div className="w-ful  relative overflow-x-auto rounded-xl bg-white p-3 overflow-y-scroll max-h-[636px h-[calc(120%-4rem)] no-scrollbar">
@@ -1611,54 +1614,14 @@ const zoomInSecond = () => {
             </>
             :
             <>
-                     <div className='flex flex-col md:w-[calc(100vw-50vw)] h-screen  p-2 gap-2'>
-                                  <div className='w-full  flex-col] gap-3 h-[200px] bg-white rounded-2xl pt-16   px-10 border-2 shadow-xl relative overflow-y-scroll '>
-                                      <div className='w-full flex justify-center shadow-2xlw- shadow-[#e36c09]   '>
-                                          <hr className="h-px border-[#e36c09] border w-32  absolute top-0 " />
-                                          <p className='absolute top-0 text-[#e36c09] text-sm'>Patient Details</p>
-                                      </div>
-                                      <div className='flex xl:flex-row flex-col justify-between  gap-5 items-center   '>
-                                          <div className='flex flex-col gap-4' >
-                                              <div className=' flex items-center flex-row w-full '>
-                                                  <label className='text-xs text-black md:w-28 w-24 text-start ' htmlFor="">Patient Name: </label>
-                                                  <p className='text-gray-500 text-xs' type="text" >Carl Blaskovish</p>
-                                              </div>
-                                              <div className=' flex items-center flex-row w-full '>
-                                                  <label className='text-xs text-black md:w-28 w-24 text-start ' htmlFor="">Rx Number: </label>
-                                                  <p className='text-gray-500 text-xs' type="text" >Carl Blaskovish</p>
-                                              </div>
-  
-                                              <div className=' flex items-center flex-row w-full '>
-                                                  <label className='text-xs text-black md:w-28 w-24 text-start ' htmlFor="">HCP Name: </label>
-                                                  <p className='text-gray-500 text-xs' type="text" >Carl Blaskovish</p>
-                                              </div>
-                                          </div>
-                                          <div className='flex flex-col gap-4'>
-                                              <div className=' flex items-center flex-row w-full '>
-                                                  <label className='text-xs text-black md:w-28 w-24 text-start ' htmlFor="">Patient Name: </label>
-                                                  <p className='text-gray-500 text-xs' type="text" >Carl Blaskovish</p>
-                                              </div>
-  
-                                              <div className=' flex items-center flex-row w-full '>
-                                                  <label className='text-xs text-black md:w-28 w-24 text-start ' htmlFor="">Rx Number: </label>
-                                                  <p className='text-gray-500 text-xs' type="text" >Carl Blaskovish</p>
-                                              </div>
-  
-                                              <div className=' flex items-center flex-row w-full '>
-                                                  <label className='text-xs text-black md:w-28 w-24 text-start ' htmlFor="">HCP Name: </label>
-                                                  <p className='text-gray-500 text-xs' type="text" >Carl Blaskovish</p>
-                                              </div>
-                                          </div>
-                                      </div>
-  
-                                  </div>
+                     <div className='flex flex-col md:w-[calc(150vw-50vw)]  p-2 gap-2'>
                                   <div className='w-full h-[calc(100vh-18rem) h-full bg-white rounded-2xl pt-20  border-2 shadow-xl relative overflow-y-scroll '>
                                       <div className='w-full flex justify-center shadow-2xlw- shadow-[#e36c09]   '>
                                           <hr className="h-px border-[#e36c09] border w-32  absolute top-0 " />
                                           <p className='absolute top-0 text-[#e36c09] text-sm'>Check List</p>
                                       </div>
   
-                                      <div className='xl:px-28 md:px-10 px-5  flex  gap-4 justify-between'>
+                                      <div className='xl:px-50 md:px-10 px-5  flex  gap-4 justify-between'>
                                           <div className=' flex flex-col gap-5'>
                                               <div className='flex items-center gap-3'>
                                                   <input
@@ -1719,10 +1682,10 @@ const zoomInSecond = () => {
                                           </div>
                                       </div>
                                       <div className='p-5'>
-                                          <p className='text-xs text-black w-28 text-start pb-2   '>Comments:</p>
-                                          <div className='w-full h-[calc(100vh-30rem)] bg-gray-100 rounded-2xl pt-20  border-2 shadow-xl relative overflow-y-scroll  '>
-  
-                                          </div>
+                                          <p className='text-xs text-black w-56 text-start pb-2   '>Comments:</p>
+                                          <input type="text" 
+                                          className='w-full h-[calc(120vh-30rem)] bg-gray-100 rounded-2xl pt-20  border-2 shadow-xl relative overflow-y-scroll  '>
+                                          </input>
                                       </div>
                                       <div className='md:px-10 px-2 pb-3 flex justify-between'>
                                           <div className='sm:w-44 csm:w-32 vsm:w-20 w-28 py-1 bg-[#00aee6] rounded-3xl flex justify-center text-xs'>Send for Re-Faxing</div>
@@ -1735,11 +1698,19 @@ const zoomInSecond = () => {
              {/*  Fax End ---------------------------*/}
              
              </div>
-               <div className='flex csm:flex-row flex-col  p-1 csm:justify-evenly justify-center items-center sm:gap-0 csm:gap-5 gap-3'>
+             {!openNetSuit?
+                <div className='flex csm:flex-row flex-col  p-1 csm:justify-evenly justify-center items-center sm:gap-0 csm:gap-5 gap-3'>
                   <div className='sm:w-44 csm:w-32 vsm:w-20 w-28 py-2 bg-[#e60000] rounded-lg flex justify-center md:text-base text-xs'>Discard</div>
                   <div className='sm:w-44 csm:w-32  vsm:w-20 w-28 py-2 bg-[#00ab06] rounded-lg flex justify-center md:text-base text-xs' onClick={handleSavePatientData}>Save</div>
                   <div className='sm:w-44 csm:w-32  vsm:w-20 w-28 py-2 bg-[#00aee6] rounded-lg flex justify-center md:text-base text-xs cursor-pointer' onClick={handle_netSuitSubmission}>Ready for Review</div>
                 </div>
+               :<>
+                <div className='flex csm:flex-row flex-col  p-1 csm:justify-evenly justify-center items-center sm:gap-0 csm:gap-5 gap-3'>
+                <div className='sm:w-44 csm:w-32  vsm:w-20 w-28 py-2 bg-[#00aee6] rounded-lg flex justify-center md:text-base text-xs cursor-pointer' onClick={handle_netDeSubmission}>Back to Case Details</div>
+                  <div className='sm:w-44 csm:w-32  vsm:w-20 w-28 py-2 bg-[#00ab06] rounded-lg flex justify-center md:text-base text-xs' onClick={handleSavePatientData}>Save</div>
+                </div>
+                </>
+              }
           </div>
         </div>
        
