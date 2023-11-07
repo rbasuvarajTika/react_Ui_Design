aadc58cd52327d7446b0556c5dd1ba8697db7174
rbasuvaraj <rbasuvaraj@tikamobile.com>
2023-11-07 22:34:14 +0530

    New Bug Fixes

--- src/components/table/FaxId_Form.jsx
+++ src/components/table/FaxId_Form.jsx
@@ -25,7 +25,9 @@ const FaxId_Form = ({ close_Form, sendFaxId }) => {
   const [isloading, setIsLoading] = useState(false)
   const [scalePopUp, setScalePopoup] = useState(1);
   const [rotate, setRotate] = useState(0);
-
+  const [showInputBoxes, setShowInputBoxes] = useState(false);
+  const [fromPage, setFromPage] = useState('');
+  const [toPage, setToPage] = useState('');
 
   const navigate = useNavigate();
   const { setOpenDuplicate, openDuplicate, showForms, setShoeForms } = useContext(DuplicateContext)
@@ -114,11 +116,26 @@ const handleRotate = () => {
   }
 }
 
+const handleSplitClick = () => {
+  setShowInputBoxes(true);
+};
+
+const handleInputChange = (e) => {
+  const { name, value } = e.target;
+  if (name === 'from') {
+    setFromPage(value);
+  } else if (name === 'to') {
+    setToPage(value);
+  }
+};
+
   return (
     <div className="fixed top-10 lg:left-48 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto ">
+      
       <div className="relative  bg-[#ffffff] rounded-2xl shadow-md shadow-gray-300  h-[calc(100vh-5rem)] w-full max-w-2xl md:pt-6 pb-10 py-3 md:pl-10 pl-5 md:pr-14 pr-10 mt-53">
         <div className='xl:w-[calc(100%-100px)] md:w-[calc(100%-150px)]  w-[calc(100%-70px)]   h-[calc(100%-100px)]  border mt-14 overflow-y-scroll absolute overflow-hidden no-scrollbar no-scrollbar  '>
           <div className=' w-full h-full  '>
+          
             <div className='text-black overflow-hidden  overflow-x-scroll overflow-y-scroll h-screen'>
             {
               !isloading ?
@@ -133,6 +150,7 @@ const handleRotate = () => {
                       height={200}
                       rotate={rotate}
                     />
+                    
                   </Document>
                 </>
                 :
@@ -149,12 +167,39 @@ const handleRotate = () => {
         <div className='relative'>
           <p className='text-[#717171] text-sm absolute top-2'>{pageNumber} of {numPages}</p>
         </div>
-
+       
         <div className='flex justify-center gap-2 mt-1'>
           <div className={`sm:w-7 sm:h-7 w-6 h-6 rounded-full  flex justify-center items-center shadow-[#00aee6] cursor-pointer sm:text-base   text-xs z-50 ${pageNumber <= 1 ? "bg-[#d9e0e3]" : "bg-[#00aee6]"}`} onClick={previousPage}> <FaArrowLeft /></div>
           <div className={`sm:w-7 sm:h-7 w-6 h-6 rounded-full bg-[#00aee6] flex justify-center items-center shadow-[#00aee6] cursor-pointer sm:text-base   text-xs z-50 ${pageNumber === numPages ? "bg-[#e7eaea]" : "bg-[#00aee6]"}`} onClick={nextPage}> <FaArrowRight /></div>
         </div>
-
+        <span className="flex flex-col gap-2 absolute top-10 md:right-1 right-2">
+                                            <div className='sm:w-20 csm:w-32 vsm:w-20 w-18 py-1 bg-[#00ab06] rounded-xl flex justify-center md:text-sm text-xs cursor-pointer' onClick={handleSplitClick}>Split</div>
+                                        </span>
+                                        {showInputBoxes && (
+        <div style={{ position: 'relative', marginTop: '10px' }}>
+          <input
+            className='bg-[#f2f2f2] rounded-2xl border border-gray-300 xl:w-[120px] text-black py-0.5 text-xs t-1'
+            type="text"
+            name="from"
+            value={fromPage}
+            onChange={handleInputChange}
+            placeholder="From Page"
+          />
+          <input
+            className='bg-[#f2f2f2] rounded-2xl border border-gray-300 xl:w-[120px] text-black py-0.5 text-xs t-1'
+            type="text"
+            name="to"
+            value={toPage}
+            onChange={handleInputChange}
+            placeholder="To Page"
+          />
+          <div style={{ position: 'absolute', top: '0', right: '240px' }}>
+            <div className='sm:w-20 csm:w-32 vsm:w-20 w-18 bg-[#00ab06] rounded-xl flex justify-center md:text-sm text-xs cursor-pointer'>
+              Save
+            </div>
+          </div>
+        </div>
+      )}
         <div className='flex flex-col gap-2 absolute top-1/2 md:right-4 right-2'>
         <div className=' rounded-lg md:w-7 w-5 h-5 md:h-7 bg-[#00aee6] flex justify-center items-center shadow shadow-[#00aee6] cursor-pointer ' onClick={handleZoomIn}> <ZoomInIcon className='md:text-base text-xs' /></div>
         <div className=' rounded-lg md:w-7 w-5 h-5 md:h-7 bg-[#00aee6] flex justify-center items-center shadow-[#00aee6] cursor-pointer' onClick={handleZoomOut}> <ZoomOutIcon className='md:text-base text-xs' /></div>
