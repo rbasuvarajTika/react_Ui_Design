f4f0713e9cacf47100e00b4fac075c100ba01f34
raghu <raghuciet@gmail.com>
2023-11-08 10:24:37 +0530

    Changes pdf api

--- src/components/table/FaxId_Form.jsx
+++ src/components/table/FaxId_Form.jsx
@@ -14,6 +14,8 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 import Loader from '../Loader';
 import axiosBaseURL from '../axios';
 import ThreeSixtyIcon from '@mui/icons-material/ThreeSixty';
+import { ToastContainer, toast } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -118,20 +120,51 @@ const handleRotate = () => {
 
 const handleSplitClick = () => {
   setShowInputBoxes(true);
+  setFromPage("");
+  setToPage("");
 };
 
 const handleInputChange = (e) => {
   const { name, value } = e.target;
-  if (name === 'from') {
+  if (name === 'fromPage') {
     setFromPage(value);
-  } else if (name === 'to') {
+  } else if (name === 'toPage') {
     setToPage(value);
   }
 };
 
+
+const handleSplitPDfRangeClick = () => {
+  // const token = localStorage.getItem('tokenTika');
+     axiosBaseURL
+       .get(`/api/v1/fax/splitPdfByRange/${sendFaxId}/${fromPage}/${toPage}`, {
+         // headers: {
+         //   Authorization: `Bearer ${token}`,
+         // },
+       })
+       .then((response) => {
+         // Handle success
+         console.log('Split PDF Successfully:', response.data);
+         if(response.data.message == 'Successfully '){
+          toast.success("PDF Splitted Sucessfully")
+          setShowInputBoxes(false);
+         }else{
+          toast.error(response.data.message)
+         }
+         // You may want to update the state or perform other actions on success.
+       })
+       .catch((error) => {
+         // Handle error
+         setShowInputBoxes(false);
+         console.error('Keep Duplicate Error:', error);
+         // You can set an error state or show an error message to the user.
+       });
+  
+ };
+
   return (
     <div className="fixed top-10 lg:left-48 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto ">
-      
+      <ToastContainer />
       <div className="relative  bg-[#ffffff] rounded-2xl shadow-md shadow-gray-300  h-[calc(100vh-5rem)] w-full max-w-2xl md:pt-6 pb-10 py-3 md:pl-10 pl-5 md:pr-14 pr-10 mt-53">
         <div className='xl:w-[calc(100%-100px)] md:w-[calc(100%-150px)]  w-[calc(100%-70px)]   h-[calc(100%-100px)]  border mt-14 overflow-y-scroll absolute overflow-hidden no-scrollbar no-scrollbar  '>
           <div className=' w-full h-full  '>
@@ -176,25 +209,25 @@ const handleInputChange = (e) => {
                                             <div className='sm:w-20 csm:w-32 vsm:w-20 w-18 py-1 bg-[#00ab06] rounded-xl flex justify-center md:text-sm text-xs cursor-pointer' onClick={handleSplitClick}>Split</div>
                                         </span>
                                         {showInputBoxes && (
-        <div style={{ position: 'relative', marginTop: '10px' }}>
+        <div style={{ position: 'relative', marginTop: '0px' }}>
           <input
-            className='bg-[#f2f2f2] rounded-2xl border border-gray-300 xl:w-[120px] text-black py-0.5 text-xs t-1'
+            className='bg-[#f2f2f2] border border-gray-300 xl:w-[100px] text-black py-0.5 text-xs t-1'
             type="text"
-            name="from"
+            name="fromPage"
             value={fromPage}
             onChange={handleInputChange}
             placeholder="From Page"
           />
           <input
-            className='bg-[#f2f2f2] rounded-2xl border border-gray-300 xl:w-[120px] text-black py-0.5 text-xs t-1'
+            className='bg-[#f2f2f2] border border-gray-300 xl:w-[100px] text-black py-0.5 text-xs t-1'
             type="text"
-            name="to"
+            name="toPage"
             value={toPage}
             onChange={handleInputChange}
             placeholder="To Page"
           />
-          <div style={{ position: 'absolute', top: '0', right: '240px' }}>
-            <div className='sm:w-20 csm:w-32 vsm:w-20 w-18 bg-[#00ab06] rounded-xl flex justify-center md:text-sm text-xs cursor-pointer'>
+          <div style={{ position: 'absolute', marginTop: '-20px', right: '280px' }}>
+            <div className='sm:w-20 csm:w-32 vsm:w-20 w-18 bg-[#00ab06] flex justify-center md:text-sm text-xs cursor-pointer' onClick={handleSplitPDfRangeClick}>
               Save
             </div>
           </div>
